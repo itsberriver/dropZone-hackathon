544c6cf80a0222e3452cd9d8256285355b0e672a
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-15 14:10:31 +0200

    modified margins from login-welcome-container

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <>
-          <div className = 'DropZone-container'>
+          <div className = 'dropZone-container'>
             <DropZone/>
 
             <Button 
