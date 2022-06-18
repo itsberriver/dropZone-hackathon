134a8335bbb8cb5fdc30ce0a04c000afa6075b27
itsberriver <jmejiaos713@gmail.com>
2022-06-18 20:32:26 +0200

    added with and height to drop images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import DropZoneComponent from "./components/DropZoneComponent"
 import LoginButton from "./components/LoginButton"
 import  { useEffect } from "react"
 import useDrivePicker from "react-google-drive-picker"
-// import React, {useCallback} from "react"
-
-
+import React from "react"
 
 function App() {
 
@@ -22,42 +20,36 @@ function App() {
 			showUploadFolders: true,
 			supportDrives: true,
 			multiselect: true,
+  });
+}
 
-			callbackFunction: (data) => {
-				if (data.action === "cancel") {
-					console.log("User clicked cancel/close button")
-				}
-				console.log(data)
-			},
-		})
-		
-		useEffect( () => {
-			if (data){
-				data.docs.map((i) => console.log(i))
-			}
-		},[data])
-	}
+useEffect( () => {
+    if (data){
+      data.docs.map((i) => console.log(i));
+    }
+  },[data])
 
 	return (
 		<div className="App">
-			<>
-				<div className = 'dropZone-container'>
-					<div className = 'green-container'>
-					</div>
-					<DropZoneComponent/>
-					<Button 
-						text = 'Subir tus archivos' /> 
-				</div>
+      <>
+          <div className = 'dropZone-container'>
+          <div className = 'green-container'>
+          </div>
+            <DropZoneComponent></DropZoneComponent>
+            <Button 
+              text = 'Subir tus archivos' /> 
+          </div>
 
-				<div className = 'login-welcome-container'>
-					<h2>Bienvenido a DDrop</h2>
-					<h6>Para subir tus archivos de forma simple a drive, puedes hacer LogIn a través de Google.</h6>
-					<hr className = 'line' />
-					<LoginButton
-						className = 'login-container'
-						text = 'Login con Google'
-						onClick = {()=>handleOpenPicker()} />
-				</div>
+          <div className = 'login-welcome-container'>
+            <h2>Bienvenido a DDrop</h2>
+            <h6>Para subir tus archivos de forma simple a drive, puedes hacer LogIn a través de Google.</h6>
+            <hr className = 'line' />
+            <LoginButton
+              className = 'login-container'
+              text = 'Login con Google'
+              onClick = {()=>handleOpenPicker()} 
+              type = "button"/>
+        </div>
 			</>
 
 		</div>
--- src/components/DropZoneComponent.jsx
+++ src/components/DropZoneComponent.jsx
@@ -1,20 +1,43 @@
-import React from "react"
 import "../stylesheets/DropZoneComponent.css"
 import FileBackground from "../images/fileBackground.png"
-// import Dropzone, {useDropzone} from "react-dropzone"
+import React, {useCallback, useState} from "react"
+import {useDropzone} from "react-dropzone"
 
-function DropZoneComponent(){
-	return(
-		<div className = 'dropzone-container'>
+
+function DropZoneComponent() {
+
+	const [chooseImage, setChooseImage] = useState([]);
+
+	const onDrop = useCallback(acceptedFiles => {
+		setChooseImage(acceptedFiles.map(file => 
+			Object.assign(file,{
+				preview: URL.createObjectURL(file) })
+		))
+	}, [])
+
+	const {getRootProps, getInputProps} = useDropzone({onDrop})
+	const choosedImages = chooseImage?.map(file => (
+        <div>
+            <img src={file.preview} style={{width:'800px', heigth: '500px'}} alt='upload pic' />
+        </div>
+    ))
+
+	return (
+		<div className = 'choosed-image-container'{...getRootProps()}>
+
+
 			<p> ARRASTRA TUS ARCHIVOS AQUÍ </p>
+
+			<input  {...getInputProps()} />
+
 			<img
 				className = 'fileBackground-image'
 				src = {FileBackground} 
 				alt = 'background for the dropzone container'/>
 
+			<form>{choosedImages}</form>
 		</div>
+
 	)
 }
-
 export default DropZoneComponent
-
